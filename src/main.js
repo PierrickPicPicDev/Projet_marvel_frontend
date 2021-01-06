32cbe140c7e3aba4ff060aa1a2703da937d0aca0
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2021-01-06 20:42:15 +0100

    add router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:9090';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: "history",
+    routes: [
+        {
+
+        },
+        {
+            path: "*",
+            redirect: '/'
+        }
+    ]
+});
+
+export default router;
